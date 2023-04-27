6c08723b86000f77df913fbcb66e8960cbe73f80
Liviu Szilagyi <szilagyiliviu@outlook.com>
2023-04-27 13:29:46 +0100

    Title

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -25,7 +25,7 @@ const AddTask = ({onAdd}) => {
     return (
     <form className='add-form' onSubmit={onSubmit}>
         <div className='form-control'>
-            <label>Task</label>
+            <label>Tasks</label>
             <input 
                 type='text' 
                 placeholder='Add Task' 
